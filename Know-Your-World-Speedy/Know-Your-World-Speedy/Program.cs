c896a629dc2c19d47f5f48f4130c6f16f5715060
RetroSabian <srajbully@retrorabbit.co.za>
2019-01-28 15:51:39 +0200

    Changed to .NET core

---
+++ Know-Your-World-Speedy/Know-Your-World-Speedy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Know_Your_World_Speedy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
